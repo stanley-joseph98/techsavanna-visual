9407020b9dc47a11116d0f335b6952f47fa8b757
Joseph Ouma <josephouma211@gmail.com>
2024-08-14 16:46:15 +0300

    Initial commit

---
+++ savannavisual.sh
@@ -0,0 +1,17 @@
+# Initialize the Git repository
+#git init
+
+# Add files to the staging area
+git add .
+
+# Commit changes to the repository
+git commit -m "Initial commit"
+
+# Rename the branch to main (if necessary)
+git branch -M main
+
+# Add the remote repository
+git remote add origin https://github.com/stanley-joseph98/techsavanna-visual
+
+# Push to the remote repository
+git push -u origin main
